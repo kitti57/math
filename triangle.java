ee82b58f39ccc42ef75d34b00479f11104e1f58e
phani <phani@gmail.com>
2018-08-23 21:16:03 +0530

    erbgh

--- triangle.java
+++ triangle.java
@@ -10,9 +10,8 @@ class AreaOfTriangle
  
          System.out.println("Enter the height of the Triangle:");
           double h= s.nextDouble();
- 
-                  //Area = (width*height)/2
+               //Area = (width*height)/2
       double area=(b*h)/2;
-      System.out.println("Area of Triangle is the final result: " + area);      
+      System.out.println("Area of Triangle is the final of result: " + area);      
    }
 }
